ccba4df493ac831e8bad8a3ec44d536661e78eb2
Cao Thanh Tung <caothanhtung@Caos-MacBook-Pro.local>
2023-06-26 17:10:19 +0900

    update session 16-problem 7

--- 1. Problems/Session 16/7. Bai 7/main.js
+++ 1. Problems/Session 16/7. Bai 7/main.js
@@ -1,20 +1,44 @@
-let inputData = document.createElement("input");
-document.body.appendChild(inputData);
-inputData.setAttribute("placeholder", "Please input book info");
-function book(
-  bookId,
-  bookName,
-  price,
-  author,
-  bookStatus,
-  inputData,
-  displayData,
-) {
-    this.bookId = bookId;
-    this.bookName = bookName;
-    this.price = price;
-    this.author = author;
-    this.bookStatus = bookStatus;
-    this.inputData = inputData;
-    this.displayData = displayData;
+let arrBook = [];
+
+class Book {
+  constructor() {
+    this.bookId = "";
+    this.bookName = "";
+    this.bookPrice = "";
+  }
+  inputData() {
+    this.bookId = document.getElementById("bookId").value;
+    this.bookName = document.getElementById("bookName").value;
+    this.bookPrice = document.getElementById("price").value;
+    document.getElementById("bookId").value = "";
+    document.getElementById("bookName").value = "";
+    document.getElementById("price").value = "";
+    // arrBook.push([this.bookId, this.bookName, this.bookPrice]);
+  }
+  displayBook() {
+    console.log("Book info: " , this.bookId , this.bookName , this.bookPrice);
+  }
+}
+
+let buttonSave = document.getElementById("save");
+buttonSave.addEventListener("click", function (event) {
+  let book = new Book();
+  book.inputData();
+  arrBook.push(book);
+  book.displayBook();
+  // displayBookInput();
+  event.preventDefault();
+  console.log(arrBook);
+});
+
+// function displayBookInput() {
+//   for (const book of arrBook) {
+//     book.displayBook();
+//   }
+// }
+
+function deleteBookFromArr() {
+  let arrIndex = Number(prompt("Please input the arr index to delete"));
+  arrBook.splice(arrIndex, 1);
+  console.log(arrBook);
 }
